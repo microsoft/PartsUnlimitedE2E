961c28c5b1e7b84e53b31d1da7db761d1965fecc
Colin Dembovsky <colindembovsky@gmail.com>
2017-01-24 20:07:49 -0700

    Adding depluralize logic

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/ProductSearch/StringContainsProductSearch.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/ProductSearch/StringContainsProductSearch.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,12 +18,36 @@ namespace PartsUnlimited.ProductSearch
 
         public async Task<IEnumerable<Product>> Search(string query)
         {
-            var lowercase_query = query.ToLower();
+			try
+			{
+				var cleanQuery = Depluralize(query);
 
-            var q = _context.Products
-                .Where(p => p.Title.ToLower().Contains(lowercase_query));
+				var q = _context.Products
+					.Where(p => p.Title.ToLower().Contains(cleanQuery));
 
-            return await q.ToListAsync();
+				return await q.ToListAsync();
+			}
+			catch
+			{
+				return new List<Product>();
+			}
         }
-    }
+
+		private string Depluralize(string query)
+		{
+			if (query.EndsWith("ies"))
+			{
+				query = query.Substring(0, query.Length - 3) + "y";
+			}
+			else if (query.EndsWith("es"))
+			{
+				query = query.Substring(0, query.Length - 1);
+			}
+			else if (query.EndsWith("s"))
+			{
+				query = query.Substring(1, query.Length);
+			}
+			return query.ToLowerInvariant();
+		}
+	}
 }
