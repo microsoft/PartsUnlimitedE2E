6b2c677cbd467e91be45c87df987e05c5488eb07
Vitalii Tsurkan <vitaliy.tsurkan2@gmail.com>
2019-12-04 14:17:18 +0200

    Category change2 Fixes #2

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    //Added some changes
     public class Category
     {
         public int CategoryId { get; set; }
