2fb5228e5dd54b1833eca9b95c2adefc452168d9
Israel Calderon <israel.calderon@accenture.com>
2020-05-02 15:54:28 -0500

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My First change.
+        // My second change.
         [Key]
         public int CartItemId { get; set; }
 
