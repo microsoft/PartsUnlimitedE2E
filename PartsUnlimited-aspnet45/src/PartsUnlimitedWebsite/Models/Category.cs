ed73910b34f3294186b87f73096709567720d41d
Zahid Malik <zahidmalik_devops@hotmail.com>
2020-12-17 08:26:07 +0000

    This is my first changes

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    
+    // This is my first change 
     public class Category
     {
         public int CategoryId { get; set; }
