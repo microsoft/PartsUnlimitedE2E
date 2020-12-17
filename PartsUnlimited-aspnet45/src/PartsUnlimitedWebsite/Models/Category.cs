da72143d21cfd93e9bf025ecb727305d407d6542
NavinTyagi <navin.tyagi@hotmail.com>
2020-12-17 14:55:59 +0400

    Update Category.cs

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     //This is my first change
+    //this is another change
     public class Category
     {
         public int CategoryId { get; set; }
