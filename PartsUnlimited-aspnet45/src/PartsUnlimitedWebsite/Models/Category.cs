10a569ac35fb66d85aa0c521deb130ed54a4975e
Ameen <ameenh@homail.com>
2020-12-17 12:43:59 +0400

    First Commnet

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    //My First Change
     public class Category
     {
         public int CategoryId { get; set; }
