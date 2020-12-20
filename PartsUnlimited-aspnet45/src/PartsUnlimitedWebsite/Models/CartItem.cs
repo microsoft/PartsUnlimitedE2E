5f9c88d04c2e492dbc439ccb774ee24855b139c0
tghazali-az <tarekghazali_az@hotmail.com>
2020-12-20 21:45:33 +0000

    change now from newfeaturess in VM

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        // change in newfeatures from VM
         [Key]
         public int CartItemId { get; set; }
 
