48de6afec7f81734d4145732f0141f9ae8a1b729
Ahmed Bisht <ahmedb@microsoft.com>
2021-03-25 21:00:36 -0700

    my commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        //sample comment
         [Key]
         public int CartItemId { get; set; }
 
--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
+        //another comment
         public int CategoryId { get; set; }
 
         [Required]
