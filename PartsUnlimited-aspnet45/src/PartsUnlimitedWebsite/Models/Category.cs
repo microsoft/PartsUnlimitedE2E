8c39a9cd3ef1eecbd497a56c5ae2bdfe444195b1
Wesley Gomes <wesleygomespaiva43@hotmail.com>
2021-05-07 15:41:52 -0300

    segunda mudança. Bug #1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        //first change 
+        //second change 
         public int CategoryId { get; set; }
 
         [Required]
