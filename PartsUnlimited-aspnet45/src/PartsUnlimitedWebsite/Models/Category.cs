f01e707214a0638be785e2d763a7278e829e72e5
Shriya <shriyabawdane@hotmail.com>
2021-05-31 13:45:02 +0530

    Category change fixes #1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        // My third change
+        // My third change for pull request
         public int CategoryId { get; set; }
 
         [Required]
