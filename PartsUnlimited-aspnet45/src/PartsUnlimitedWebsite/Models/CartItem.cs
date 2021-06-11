c3111f79650b62b67143eb2a89b7a4b11dc625c9
rsalve <r.salve@accenture.com>
2021-06-11 13:18:28 +0530

    Added Comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My third change
+        //My third change.
         [Key]
         public int CartItemId { get; set; }
 
