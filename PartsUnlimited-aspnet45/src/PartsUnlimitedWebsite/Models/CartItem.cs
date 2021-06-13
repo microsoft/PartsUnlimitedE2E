84027571b8dcfb1b7484bdf1f811d60b4700fb60
Pratheeksha Damle <pratheeksha.damle@accenture.com>
2021-06-13 17:05:50 +0530

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -8,7 +8,7 @@ namespace PartsUnlimited.Models
         //My first change
         [Key]
         public int CartItemId { get; set; }
-        //My second change
+        //My second change.
         [Required]
         public string CartId { get; set; }
         public int ProductId { get; set; }
