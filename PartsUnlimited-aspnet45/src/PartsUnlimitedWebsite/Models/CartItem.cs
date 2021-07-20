21dde35cbdb222d8123bc2e91f43a68e6f5005f2
Nipuna Rambukkanage <johndoe@example.comnipunaram@gmail.com>
2021-07-21 05:07:34 +0530

    Added Comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My First Change
+        //My SECONDEEEE Change
         [Key]
         public int CartItemId { get; set; }
 
