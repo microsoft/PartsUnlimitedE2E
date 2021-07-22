6c08195fc34be66fd3370bc3bf26236393572686
LuisG02 <84988831+LuisG02@users.noreply.github.com>
2021-07-22 16:03:46 -0600

    Update CartItem.cs

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -6,7 +6,6 @@ namespace PartsUnlimited.Models
     public class CartItem : ILineItem
     {
         //Mi segundo cambio
-        //Este cambio realizado en GitHub debe verse en el repositorio ya que se va a utilizar el comando Git Fetch Prune 
         [Key]
         public int CartItemId { get; set; }
 
