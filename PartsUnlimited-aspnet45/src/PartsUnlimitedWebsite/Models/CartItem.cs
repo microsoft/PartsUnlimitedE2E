ec69197c2e4f5e6c42bbd62418ad1641c13f1ba8
Shweta <Shweta@demomail.com>
2021-08-24 12:16:55 +0530

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My first change
+        //My Second change
         [Key]
         public int CartItemId { get; set; }
 
