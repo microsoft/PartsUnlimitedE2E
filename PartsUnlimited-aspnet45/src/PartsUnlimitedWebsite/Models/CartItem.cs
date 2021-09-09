1010b163326170096c01bc05286970210df3f23e
pdvprasad-sme <pdvprasad.obiee@gmail.com>
2021-09-09 07:33:05 +0530

    Added Commands

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
-    {// My First Change
+    {// My Second Change
         [Key]
         public int CartItemId { get; set; }
 
--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class Category
-    {
+    {// My Third Change
         public int CategoryId { get; set; }
 
         [Required]
