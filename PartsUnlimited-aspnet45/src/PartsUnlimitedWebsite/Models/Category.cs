ff65a8746d7f758be8fb6a976ec4539bf9b90e2b
Julio Hernandez <julio.b.hernandez@accenture.com>
2021-10-16 00:29:35 -0500

    Category change. Fixes #1.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        // My third change.
+        // My third changem
         public int CategoryId { get; set; }
 
         [Required]
