046d7384a99efb4e161c182b8bcc78f4ddeb38fd
Marcelo Guillermo <marcelo.guillermo@accenture.com>
2021-10-18 10:12:47 -0500

    Va por mi parte hasta GitHub

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CommunityPost.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CommunityPost.cs
@@ -9,6 +9,7 @@ namespace PartsUnlimited.Models
         public string Content { get; set; }
         public DateTime DatePosted { get; set; }
         public CommunitySource Source { get; set; }
+        // Este cambko va por mi parte hasta github
     }
 
     public enum CommunitySource
