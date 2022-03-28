faafb8a665c16d45274afe9af64b692781f7792a
Abdessamad Traibiz <abdessamad.traibiz@gmail.com>
2022-03-28 09:47:47 +0000

    Added comment.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My first change
+        //My second change.
         [Key]
         public int CartItemId { get; set; }
 
