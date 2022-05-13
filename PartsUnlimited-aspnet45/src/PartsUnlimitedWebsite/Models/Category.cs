605b271da57a64e04a9df041eb358a5600ecf709
Deen Mohammad <deen.6eee@gmail.com>
2022-05-13 15:46:18 +1000

    Category change.Fixes AB#3

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -14,6 +14,6 @@ namespace PartsUnlimited.Models
 
         public string ImageUrl { get; set; }
 
-        public List<Product> Products { get; set; }
+        public List<Product> Products { get; set; }//done 
     }
 }
