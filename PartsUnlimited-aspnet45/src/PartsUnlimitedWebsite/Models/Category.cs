84985ed45aa4cef7a47d103e53ef82fe444c58bb
Kristof Swinnen <kristof@slsconsult.be>
2022-05-20 15:40:39 +0200

    comment 1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // wijziging in release
     public class Category
     {
         public int CategoryId { get; set; }
