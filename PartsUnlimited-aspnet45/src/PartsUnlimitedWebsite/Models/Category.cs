d440edb82ede483b4d7ff6830d773622a06786ae
Emilio Valdivia <evaldiviaci@unsa.edu.pe>
2022-07-07 23:19:45 -0500

    Category change. Fixes #1.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -6,7 +6,7 @@ namespace PartsUnlimited.Models
     public class Category
     {
         public int CategoryId { get; set; }
-
+        //cambio
         [Required]
         public string Name { get; set; }
 
@@ -17,3 +17,4 @@ namespace PartsUnlimited.Models
         public List<Product> Products { get; set; }
     }
 }
+
