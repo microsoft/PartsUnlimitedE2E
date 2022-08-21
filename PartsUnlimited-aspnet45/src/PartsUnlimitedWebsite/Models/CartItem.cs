7bfd0da0ba6352f6292bb328031201fec7303444
Bhuvana Gayathri <kbgayathri0915@gmail.com>
2022-08-21 10:28:40 +0530

    My commit.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
+        // My first change "20jg1a0537"
         [Key]
         public int CartItemId { get; set; }
 
