f61af26a0aeecdc6a6aea51f0b2c33e9485eaae3
Marcel Andrea Rosales Perez Rodriguez <marrodriguez@pulte.com>
2022-10-20 11:20:53 -0600

    Category change. Fixes #1.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // My Second Change.
     public class Category
     {
         public int CategoryId { get; set; }
