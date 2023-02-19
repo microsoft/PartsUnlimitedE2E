54990719faf1f4b4d001d456c5430368fd3bfdf6
Jessica Hernandez <melendez-hernandez@hotmail.com>
2023-02-19 01:33:41 -0600

    My commit with github exercise

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // Cart item model with properties
     public class CartItem : ILineItem
     {
         [Key]
