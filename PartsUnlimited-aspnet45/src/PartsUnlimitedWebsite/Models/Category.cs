a4d4bca8aa9c509e4ed63ddfce2db8b65252d927
Levago <jorgel.marmolejo96@hotmail.com>
2023-04-05 08:04:09 -0600

    Added Coments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        //My Thrid Change.
+        //My Third Change
         public int CategoryId { get; set; }
 
         [Required]
