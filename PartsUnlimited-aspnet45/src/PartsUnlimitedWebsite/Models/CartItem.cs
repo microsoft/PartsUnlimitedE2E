33d279b05785ab7883ed680815c225e3ce2985dd
ARMANOD <stickarmand11@gmail.com>
2023-04-05 14:56:35 -0600

    Releace

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //my second change.
+        //my fist change.
         [Key]
         public int CartItemId { get; set; }
 
