5dcfce373264b208b5de5af81f83d6548d708659
Roberto Friber <robertofriber@gmail.com>
2023-04-05 16:58:30 -0600

    Category Change. Fixes #1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        //Mi tercer cambio
+        //Mi tercer cambio 
         public int CategoryId { get; set; }
 
         [Required]
