328a507835973cb96fe4b7d4f8ec32a03980a5cd
Levago <jorgel.marmolejo96@hotmail.com>
2023-04-05 09:34:57 -0600

    Category change. Fixes #1

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        //My Third Change
+        //Category change. Fixes #1
         public int CategoryId { get; set; }
 
         [Required]
