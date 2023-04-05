97c912f032f625852e2ddc801686d5f3acfa5211
Roberto Friber <robertofriber@gmail.com>
2023-04-05 16:49:26 -0600

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //Un cambio
+        //Un segundo cambio
         [Key]
         public int CartItemId { get; set; }
 
