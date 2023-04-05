7f2646b177d584bc8b9ed8683ee0c307373d1f9c
PérezCalzadaDavidAlejandro <180227@utags.edu.mx>
2023-04-04 19:29:13 -0600

    Added Comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My First Change.
+        //My Second Change.
         [Key]
         public int CartItemId { get; set; }
 
