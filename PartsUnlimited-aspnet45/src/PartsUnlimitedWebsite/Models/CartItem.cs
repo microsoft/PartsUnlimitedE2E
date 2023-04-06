3530d2b7b9dbda7325b0c23665bbbb87a1532d0d
Levago <jorgel.marmolejo96@hotmail.com>
2023-04-05 19:20:44 -0600

    aaaaa

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,11 +5,8 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-<<<<<<< HEAD
         //My Second Change
-=======
         //My First Change
->>>>>>> 262ac993372e98271bd948c98d3b1618db637b3f
         [Key]
         public int CartItemId { get; set; }
 
