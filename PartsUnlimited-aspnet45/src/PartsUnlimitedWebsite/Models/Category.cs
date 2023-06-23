4ec2470b4a85e61cb160222503b751c9253fe00a
Lilykappiah <134174703+Lilykappiah@users.noreply.github.com>
2023-06-23 02:49:44 +0000

    Category change. Fixes #5

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-       // My third change.
+       // My third changes for the day to make it work.
         public int CategoryId { get; set; }
 
         [Required]
