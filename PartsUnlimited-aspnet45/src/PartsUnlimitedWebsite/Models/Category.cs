d7f3f298f0ab783be665286f420b38f7317d000e
mohamed-mbarek <mbarekmohamed99@gmail.com>
2023-11-02 00:04:31 +0100

    Category change. Fixes #3

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        // This is my 4 change 
+        // This is my Fix Bug
         public int CategoryId { get; set; }
 
         [Required]
