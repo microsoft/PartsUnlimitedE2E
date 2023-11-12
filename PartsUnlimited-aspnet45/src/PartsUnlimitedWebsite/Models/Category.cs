015416811bf16006faad5afb73a152057b5ae714
Hassen BENGAMRA <etudiant.bengamra.hassen@uvt.tn>
2023-11-12 15:36:57 +0100

    Category change. Fixes #1-2

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
-        // My Fouth Change
+        // My Fifth Change
         public int CategoryId { get; set; }
 
         [Required]
