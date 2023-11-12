ebc7fd43d20bee1319dc1eb3bc057d7020d84e71
Hassen BENGAMRA <etudiant.bengamra.hassen@uvt.tn>
2023-11-12 11:13:55 +0100

    Added Comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,8 +5,8 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My First Change
-        
+        // My Second Change
+
         [Key]
         public int CartItemId { get; set; }
 
