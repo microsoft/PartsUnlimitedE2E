e1f213aa0dc283670a54882d72f7000eb2493c7b
Timotei Iancu <timoteimarius.19@gmail.com>
2023-12-19 17:47:31 +0200

    release

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -13,7 +13,7 @@ namespace PartsUnlimited.Models
         public string CartId { get; set; }
         public int ProductId { get; set; }
         public int Count { get; set; }
-
+//ready
         [DataType(DataType.DateTime)]
         public DateTime DateCreated { get; set; }
 
